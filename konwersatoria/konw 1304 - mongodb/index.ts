c4baee171fd21474233519bfd9a2ed3a13937620
Rysiek Brzegowy <r.brzegowy@gmail.com>
2022-04-19 14:39:19 +0200

    mongodb - przykłady z konw + .watch()

---
+++ konwersatoria/konw 1304 - mongodb/index.ts
@@ -0,0 +1,69 @@
+
+import { ChangeStreamDocument } from "mongodb";
+import mongoose from "mongoose";
+
+// connection string
+const connString = 'mongodb+srv://pabapp:pabapp1@programowanieaplikacjib.gidyy.mongodb.net/pab?retryWrites=true&w=majority'
+
+
+// model dokumentu
+const notesSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+    private: Boolean,
+    tags: [String]
+}, {
+    timestamps: true
+})
+
+main()
+async function main() {
+    // 1. Przygotowanie komunikacji - połączenie z bazą danych
+    console.log('Connecting to mongo');
+    const db = await mongoose.connect(connString)
+    console.log('Mongo Connected!')
+
+    // 2. Przygotowanie komunikacji - tworzenie schema z modelu
+    const noteModel = mongoose.model('notes', notesSchema)
+
+    // 3. Akcje - dodawanie wpisu
+    const newNote = new noteModel({
+        title: 'New note',
+        content: 'From mongoose',
+        private: true,
+        tags: ['Friday', 'Yellow', 'Kebab']
+    })
+
+    // 4. AKCJE - zapis
+    const saveRet = await newNote.save(); // także .update(), .updateMany(), .validate()
+    console.log('SAVE - new note id: ', newNote.id);
+
+    // 5. AKCJE - aktualizacja wpisu
+    newNote.title = `zaktualizowana notatka ${newNote.id}`
+    await newNote.save();
+    console.log('UPDATE - new note updated title: ', newNote.title);
+    // wyszukanie i aktualizacja wpisu: .findOneAndUpdate()
+    // nadpisywanie całego dokumentu.overwrite({data}).save()
+
+    // 6. AKCJE - usuwanie wpisu
+    const delRet = await noteModel.deleteMany({ private: false }) // także .deleteMany()
+    console.log('NOTE DELETE,', delRet)
+
+    // 7. AKCJE - odpytywanie o kolekcję
+    // const notes = await noteModel.find();
+    // console.log(`NOTES Colection ${notes.length}:`, notes)
+
+    // mongoose queries: https://mongoosejs.com/docs/queries.html
+
+    // 8. WATCH - "zapisanie się" na zmiany w kolekcji (analogicznie w dokumencie)
+    const notesEvents = noteModel.watch()
+    notesEvents.on('change', change => onNotesChange(change))
+
+    // UPDATE kolekcji po zapisaniu się na zmiany w kolekcji (żeby zobaczyć że .watch działa)
+    newNote.title = `zaktualizowana notatka ${new Date().toLocaleString()}`
+    await newNote.save();
+
+}
+function onNotesChange(notes: ChangeStreamDocument) {
+    console.log('NOTES CHANGE!', notes)
+}
